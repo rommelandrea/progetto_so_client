f922d92420e1576a462be4825b48cc924e0c17a0
Andrea Romanello <rommelandrea@gmail.com>
2012-07-18 15:40:15 +0200

    allocazione lettura e chiusura del socket su 3 funzioni

--- client.c
+++ client.c
@@ -5,23 +5,22 @@
  *      Author: rommel
  */
 
-
 #include "header_proj.h"
 
-void recive_socket(){
-	int sd = -1, sd2 = -1, n;
-	char buf[200];
-	char sock[20];
-
+/**
+ * crea un socket
+ */
+void create_socket(int *sd) {
+	printf("SD %d\n", *sd);
+	int ret;
 	struct sockaddr_un srvaddr;
+	char sock[20];
 
 	int pid = getpid();
 	sprintf(sock, "/tmp/%d.sock", pid);
 
-	printf("\nSOCKCLI %s\n", sock);
-
-	sd = socket(AF_UNIX, SOCK_STREAM, 0);
-	if (sd < 0) {
+	*sd = (int)socket(AF_UNIX, SOCK_STREAM, 0);
+	if (*sd < 0) {
 		perror("Unable to open server socket");
 		exit(1);
 	}
@@ -30,53 +29,75 @@ void recive_socket(){
 	srvaddr.sun_family = AF_UNIX;
 	strcpy(srvaddr.sun_path, sock);
 
-	n = bind(sd, (struct sockaddr *)&srvaddr, sizeof(srvaddr));
-	if (n < 0) {
+	ret = bind(*sd, (struct sockaddr *) &srvaddr, sizeof(srvaddr));
+	if (ret < 0) {
 		perror("Unable to bind socket server");
+		close(*sd);
 		exit(1);
 	}
 
-	n = listen(sd, 1);
-	if (n < 0) {
+	ret = listen(*sd, 1);
+	if (ret < 0) {
 		perror("Unable to listen socket server");
 		exit(1);
 	}
+}
 
-	sd2 = accept(sd, NULL, NULL );
+void close_socket(int *s, int p){
+	char sock[20];
+	sprintf(sock, "/tmp/%d.sock", p);
+
+	close(*s);
+	unlink(sock);
+}
+
+/**
+ * questa funzione serve per leggere il socket in ingresso
+ */
+void recive_socket(int *sd) {
+	int sd2 = -1, ret;
+	char buf[200];
+
+	sd2 = accept(*sd, NULL, NULL );
 	if (sd2 < 0) {
 		perror("Unable to accept socket server");
 		exit(1);
 	}
 
-	n = read(sd2, buf, sizeof(char)*200);
-	if(n < 0){
+	if (read(sd2, buf, sizeof(char) * 200) < 0) {
 		perror("Unable st read on socket");
 		exit(1);
 	}
 
 	printf("\n%s\n", buf);
-	//strcpy(buf, "dati server");
-	//n = read(sd2, buf, sizeof("dati server"));
 
-	close(sd);
+	close(*sd);
 	close(sd2);
-	unlink(sock);
 }
 
-int main(int argc, char **argv){
+int main(int argc, char **argv) {
 	int MSG_Q__main_bus;
 	request *richiesta = malloc(sizeof(request));
 	response *risposta = malloc(sizeof(response));
 
+	int sock_id;
+	sock_id = 0;
+
 	MSG_Q__main_bus = msgget(SERVER_KEY, 0);
-	if(MSG_Q__main_bus < 0 ){
+	if (MSG_Q__main_bus < 0) {
 		perror("The message queue don't exist");
 		exit(1);
 	}
 
+	/**
+	 * apro il socket
+	 */
+	create_socket(&sock_id);
+
+
 	richiesta->mtype = TOSRV;
 	richiesta->clientId = getpid();
-	richiesta->priority = ILLWAIT;
+	richiesta->priority = IMONHOLODAY;
 	richiesta->kindof_service = OPHTHALMOLOGY;
 	richiesta->turn = 0;
 	richiesta->price = 0;
@@ -84,14 +105,16 @@ int main(int argc, char **argv){
 	msgsnd(MSG_Q__main_bus, richiesta, sizeof(request), TOSRV);
 	printf("richiesta inviata\n");
 
+	recive_socket(&sock_id);
+
 	printf("in attesa della risposta");
 
 	msgrcv(MSG_Q__main_bus, risposta, sizeof(response), TOCLI, 0);
-	//printf("il mio pid %d\n", getpid());
 
-	recive_socket();
+	printf("ricevuta, ho il turno: %d\n", risposta->turn);
 
 	free(richiesta);
+	close_socket(&sock_id, getpid());
 
 	return 0;
 }
